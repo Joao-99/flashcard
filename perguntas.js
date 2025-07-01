cc02d85a0226a5d7bcbd41bee445146595e299cd
João1409 <joao.lucas.cavalcante@escola.pr.gov.br>
2025-07-01 10:36:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Motorzão',
+    'Qual moto é melhor para montar projetinho',
+    '150'
+)
+
+criaCartao(
+    'Motorzão',
+    'qual moto é conhecida por ser uma da melhores da honda?',
+    'É a cg 150 carburada de 2004 a 2008'
+)
+
+criaCartao(
+    'Motorzão',
+    'Qual a preparação mais mentirosa dos racha',
+    'É o 4mm '
+)
+
+criaCartao(
+    'Motorzão',
+    'o que faz o motor queimar combustivel',
+    'A vela'
+)
